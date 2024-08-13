01584e570fd68712bc9403fbbd9f95d82b4fbeea
JackS0l0 <you@example.com>
2024-08-14 00:22:52 +0400

    modified:   app/__pycache__/urls.cpython-312.pyc 	modified:   app/urls.py 	modified:   articles/__pycache__/views.cpython-312.pyc 	modified:   articles/views.py 	modified:   db.sqlite3 	modified:   static/css/style.css 	modified:   static/css/style.css.map 	modified:   static/css/style.scss 	modified:   staticfiles/css/style.css 	modified:   staticfiles/css/style.css.map 	modified:   staticfiles/css/style.scss 	new file:   templates/about.html 	modified:   templates/addons/footer.html 	new file:   templates/contact.html 	new file:   templates/partnors.html

--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import DetailView,ListView
-from .models import Articles, Categories
+from .models import Articles, Categories,Contact,About,Partnors
 from django.shortcuts import get_object_or_404
 class ArticleDetail(DetailView):
     model = Articles
@@ -17,18 +17,15 @@ class CategoriesDetail(ListView):
     template_name = 'category.html'
     context_object_name = 'articles'  # Articles will be referred as 'articles' in the template
     paginate_by = 6
-
     def get_context_data(self, **kwargs):
         data = super(CategoriesDetail, self).get_context_data(**kwargs)
         data['title'] = "Time's Person"
         data['categories'] = Categories.objects.all().order_by('-date')
         data['categories_navbar'] = Categories.objects.filter(navbar=True).order_by('-date')
-
         category_id = self.kwargs.get('category_id')
         category = get_object_or_404(Categories, id=category_id)
         data['category_name'] = category.name
         return data
-
     def get_queryset(self):
         category_id = self.kwargs.get('category_id')
         if category_id:
@@ -37,3 +34,27 @@ class CategoriesDetail(ListView):
         else:
             queryset = Articles.objects.all().order_by('-date')
         return queryset
+def about(request):
+    data={
+        'title':"Time's Person - About",
+        'about':About.objects.all()[0:1],
+        'categories' : Categories.objects.all().order_by('-date'),
+        'categories_navbar':Categories.objects.filter(navbar=True).order_by('-date')
+    }
+    return render(request,'about.html',data)
+def contact(request):
+    data={
+        'title':"Time's Person - Contact",
+        'contact':Contact.objects.all()[0:1],
+        'categories' : Categories.objects.all().order_by('-date'),
+        'categories_navbar':Categories.objects.filter(navbar=True).order_by('-date')
+    }
+    return render(request,'contact.html',data)
+def partnors(request):
+    data={
+        'title':"Time's Person - Partners",
+        'partnors':Partnors.objects.all().order_by('-date'),
+        'categories' : Categories.objects.all().order_by('-date'),
+        'categories_navbar':Categories.objects.filter(navbar=True).order_by('-date')
+    }
+    return render(request,'partnors.html',data)
