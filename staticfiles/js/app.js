2425fe066ac3a39e45fc50df8667e7c5ea37b77b
JackS0l0 <you@example.com>
2024-08-13 18:39:42 +0400

    modified:   locale/az/LC_MESSAGES/django.mo 	modified:   locale/az/LC_MESSAGES/django.po 	modified:   locale/en/LC_MESSAGES/django.po 	modified:   locale/ru/LC_MESSAGES/django.mo 	modified:   locale/ru/LC_MESSAGES/django.po 	modified:   static/css/style.css 	modified:   static/css/style.css.map 	modified:   static/css/style.scss 	modified:   static/js/app.js 	modified:   staticfiles/css/style.css 	modified:   staticfiles/css/style.css.map 	modified:   staticfiles/css/style.scss 	modified:   staticfiles/js/app.js 	modified:   templates/addons/header.html 	modified:   templates/index.html

--- staticfiles/js/app.js
+++ staticfiles/js/app.js
@@ -12,5 +12,14 @@ function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
-//welcomeCarusel
-  
+if (localStorage.getItem('theme') === 'dark') {
+    document.body.classList.add('dark-mode');
+}
+document.getElementById('theme-toggle-button').addEventListener('click', function () {
+    document.body.classList.toggle('dark-mode');
+    if (document.body.classList.contains('dark-mode')) {
+        localStorage.setItem('theme', 'dark');
+    } else {
+        localStorage.setItem('theme', 'light');
+    }
+});
