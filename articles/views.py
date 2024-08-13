aee4658315a6adc9d3948d01bbbdd6f7dd0ecade
JackS0l0 <you@example.com>
2024-08-13 19:33:13 +0400

    modified:   app/__pycache__/urls.cpython-312.pyc 	modified:   articles/__pycache__/views.cpython-312.pyc 	modified:   articles/views.py 	modified:   locale/az/LC_MESSAGES/django.mo 	modified:   locale/az/LC_MESSAGES/django.po 	modified:   locale/en/LC_MESSAGES/django.po 	modified:   locale/ru/LC_MESSAGES/django.po 	modified:   static/css/style.css 	modified:   static/css/style.css.map 	modified:   static/css/style.scss 	modified:   staticfiles/css/style.css 	modified:   staticfiles/css/style.css.map 	modified:   staticfiles/css/style.scss 	modified:   templates/category.html

--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import DetailView,ListView
 from .models import Articles, Categories
+from django.shortcuts import get_object_or_404
 class ArticleDetail(DetailView):
     model = Articles
     template_name = 'post.html'
@@ -12,16 +13,27 @@ class ArticleDetail(DetailView):
         data['categories_navbar']=Categories.objects.filter(navbar=True).order_by('-date')
         return data
 class CategoriesDetail(ListView):
-    model = Categories
+    model = Articles
     template_name = 'category.html'
-    context_object_name = 'categories'
+    context_object_name = 'articles'  # Articles will be referred as 'articles' in the template
+    paginate_by = 6
+
     def get_context_data(self, **kwargs):
-        data=super(CategoriesDetail, self).get_context_data(**kwargs)
-        data['title']="Time's Person"
-        data['articles']=Articles.objects.all().order_by('-date')
-        data['categories']=Categories.objects.all().order_by('-date')
-        data['categories_navbar']=Categories.objects.filter(navbar=True).order_by('-date')
+        data = super(CategoriesDetail, self).get_context_data(**kwargs)
+        data['title'] = "Time's Person"
+        data['categories'] = Categories.objects.all().order_by('-date')
+        data['categories_navbar'] = Categories.objects.filter(navbar=True).order_by('-date')
+
         category_id = self.kwargs.get('category_id')
-        category = Categories.objects.get(id=category_id)
+        category = get_object_or_404(Categories, id=category_id)
         data['category_name'] = category.name
         return data
+
+    def get_queryset(self):
+        category_id = self.kwargs.get('category_id')
+        if category_id:
+            category = get_object_or_404(Categories, id=category_id)
+            queryset = Articles.objects.filter(category=category).order_by('-date')
+        else:
+            queryset = Articles.objects.all().order_by('-date')
+        return queryset
